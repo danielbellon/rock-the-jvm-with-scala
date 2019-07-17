d086c516ac029dfd307569919f665b3f46e849b8
Daniel Bellón <daniel.bellon@payulatam.com>
2019-07-17 11:39:34 -0500

    Add first lecture about variables

---
+++ src/lectures/part1basics/ValuesAndVariablesTypes.scala
@@ -0,0 +1,30 @@
+package lectures.part1basics
+
+object ValuesAndVariablesTypes extends App {
+
+  // -------------------------------------------------------------------------------------------------------------------
+  // Values
+  // -------------------------------------------------------------------------------------------------------------------
+
+  // Variables are immutable
+  val x = 42
+  println(x)
+  // x = 4
+
+  val stringValue: String = "something"
+  val anotherStringValue = "another thing"
+
+  val booleanValue: Boolean = false
+  val charValue: Char = 'A'
+  val intValue: Int = 23423
+  val longValue: Long = 342348053453405930L
+
+  // -------------------------------------------------------------------------------------------------------------------
+  // Variables are NOT immutable
+  // -------------------------------------------------------------------------------------------------------------------
+
+  var intVariable = 43645
+  intVariable += 4564565
+  print(intVariable)
+
+}
---
+++ src/playground/JavaPlayground.java
@@ -0,0 +1,11 @@
+package playground;
+
+/**
+ * @author Daniel Bellón
+ */
+public class JavaPlayground {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+}
